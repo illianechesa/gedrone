790f3d6a79e49c885640f16871d3918a9878fe84
Illia Nechesa <nechesaillia@gmail.com>
2020-06-07 15:23:12 +0200

    add delete pop up for drone and flight & update some refs
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/homecrud.js
+++ js/homecrud.js
@@ -5,3 +5,23 @@ function editDrone() {
 function editFlight() {
     window.open("editflight.html", "_self");
 }
+
+function deleteDrone() {
+    var txt;
+    var r = confirm("¿Estás seguro que deseas eliminar esta aeronave?");
+    if (r == true) {
+        txt = "Se ha eliminado la aeronave";
+    } else {
+        txt = "Cancelado";
+    }
+}
+
+function deleteFlight() {
+    var txt;
+    var r = confirm("¿Estás seguro que deseas eliminar este vuelo?");
+    if (r == true) {
+        txt = "Se ha eliminado el vuelo";
+    } else {
+        txt = "Cancelado";
+    }
+}
