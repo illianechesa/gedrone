fe1b17563a015269a11233f5eef28023ee3b9ade
Illia Nechesa <nechesaillia@gmail.com>
2020-06-11 18:47:40 +0200

    add javascript to addmodel view and maintenance part
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/homecrud.js
+++ js/homecrud.js
@@ -36,6 +36,14 @@ function addDrone() {
 }
 
 function loadModel() {
-    document.getElementById("aeronaves").innerHTML = '<tr> <th>GF</th> <td>Ala</td> <td>1</td> </tr> <tr> <th>M</th> <td>Motor 1</td> <td>2</td> </tr> <tr> <th>M</th> <td>Motor 2</td> <td>3</td> </tr> <tr> <th>V</th> <td>Hélice</td> <td>4</td> </tr> <tr> <th>M</th> <td>Corcho</td> <td>5</td> </tr>';
+    document.getElementById("aeronaves").innerHTML = '<strong>Listado</strong><tr> <th>Grupo</th> <th>Componente</th> <th>ID</th> </tr> <tr> <th>GF</th> <td><a href="#" onclick="loadMto()">Ala</a></td> <td>1</td> </tr> <tr> <th>M</th> <td><a href="#" onclick="loadMto()">Motor 1</a></td> <td>2</td> </tr> <tr> <th>M</th> <td><a href="#" onclick="loadMto()">Motor 2</a></td> <td>3</td> </tr> <tr> <th>V</th> <td><a href="#" onclick="loadMto()">Hélice</a></td> <td>4</td> </tr> <tr> <th>M</th> <td><a href="#" onclick="loadMto()">Corcho</a></td> <td>5</td> </tr>';
 }
 
+function loadMto() {
+    document.getElementById("vuelos").innerHTML = '<strong>Mantenimineto</strong> <tr> <th>Aeronave</th> <th>ID</th> <th>Numero Serie</th> <th>Horas funcionando</th> <th>Fecha próxima revision</th> </tr> <tr> <th>A1</th> <td>1</td> <td>34223</td> <td>12h 30min</td> <td>22/07/2020</td> </tr> <tr> <th>A2</th> <td>1</td> <td>3324</td> <td>1h 10min</td> <td>02/07/2020</td> </tr> <tr> <th>A3</th> <td>1</td> <td>1234</td> <td>30h 30min</td> <td>01/07/2020</td> </tr> <tr> <th>A4</th> <td>1</td> <td>2313</td> <td>1h 15min</td> <td>22/07/2020</td> </tr> <tr> <th>A5</th> <td>1</td> <td>78568</td> <td>2h 30min</td> <td>22/07/2020</td> </tr> <tr> <th><button onclick="hidemto()">Esconder MTO</button></th> </tr>';
+    document.getElementById("vuelos").style.visibility = "visible"
+}
+
+function hidemto() {
+    document.getElementById("vuelos").style.visibility = "hidden";
+}
