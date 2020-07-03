5588478b1466473d8027324de27b3cda69071979
Illia Nechesa <nechesaillia@gmail.com>
2020-07-03 11:29:23 +0200

    show hide drones implementation
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/homecrud.js
+++ js/homecrud.js
@@ -222,6 +222,18 @@ function showhidemodels() {
     }
 }
 
+function showhideDrones() {
+    var x = document.getElementById("tablamodelos");
+    if (x.style.visibility === "hidden") {
+        x.style.visibility = "visible";
+        x.style.display = "block"
+    }
+    else {
+        x.style.visibility = "hidden";
+        x.style.display = "none";
+    }
+}
+
 function addNewModel() {
     var x = document.getElementById("newmodel");
     x.innerHTML = '<h6 style="width: 40%; margin-top: 30px; margin-left: 150px; margin-right: 0px;">Para añadir un nuevo modelo, adjunte un excel</h6> <input style="width: 80%; margin-left: 0px; margin-top: 30px;" type="file" name="adjunto" accept=".xlsx,.xlsm" /> <button style="margin-top: 30px; margin-left: 0px; width: 80px; height: 30px;" onclick="loadModel()" style="color: black;">Cargar</button>';
