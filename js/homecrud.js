b359bd608c8e873e1e182cb35c648d8d80e202a0
Illia Nechesa <nechesaillia@gmail.com>
2020-07-10 20:07:30 +0200

    add history to the incident
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/homecrud.js
+++ js/homecrud.js
@@ -263,3 +263,23 @@ function addIncident() {
         x.style.display = "none";
     }
 }
+
+function showIncidentHistory() {
+    var modal = document.getElementById('modal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
