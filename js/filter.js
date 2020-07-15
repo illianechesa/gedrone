b3ecfa01b3d06ce2a691b5559cb91b04cb0a6654
Illia Nechesa <nechesaillia@gmail.com>
2020-07-15 12:47:04 +0200

    improve the filters in the index menu
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/filter.js
+++ js/filter.js
@@ -25,28 +25,6 @@ function searchProject() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("inputProject");
     filter = input.value.toUpperCase();
-    table = document.getElementById("vuelos");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[3];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-
-function searchProject() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("inputProject");
-    filter = input.value.toUpperCase();
     table = document.getElementById("persona");
     tr = table.getElementsByTagName("tr");
 
@@ -73,7 +51,7 @@ function searchIncidents() {
     tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
+    for (i = 2; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
             txtValue = td.textContent || td.innerText;
