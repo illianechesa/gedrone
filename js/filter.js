08ab6ce3e3a061e53a60ba132e8038def44a2dfd
Illia Nechesa <nechesaillia@gmail.com>
2020-07-17 13:00:28 +0200

    filter by all columns project index
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,6 @@
 function searchProject() {
     // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td0, td1, td2, td3, i, txtValue;
     input = document.getElementById("inputProject");
     filter = input.value.toUpperCase();
     table = document.getElementById("persona");
@@ -8,10 +8,16 @@ function searchProject() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        td0 = tr[i].getElementsByTagName("td")[0];
+        td1 = tr[i].getElementsByTagName("td")[1];
+        td2 = tr[i].getElementsByTagName("td")[2];
+        td3 = tr[i].getElementsByTagName("td")[3];
+        if (td0 || td1 || td2 || td3) {
+            txtValue0 = td0.textContent || td0.innerText;
+            txtValue1 = td1.textContent || td1.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+            txtValue3 = td3.textContent || td3.innerText;
+            if (txtValue0.toUpperCase().indexOf(filter) > -1 || txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1 || txtValue3.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
