8b6340dd1f3e334c35f28e8d0bc77e533769fdb4
Illia Nechesa <nechesaillia@gmail.com>
2020-07-21 12:00:22 +0200

    add observador as a new role
    Signed-off-by: Illia Nechesa <nechesaillia@gmail.com>

--- js/homecrud.js
+++ js/homecrud.js
@@ -201,6 +201,8 @@ function showPiloto() {
     document.getElementById("menu4").style.visibility = "hidden";
     document.getElementById("menu5").innerHTML = "";
     document.getElementById("menu5").style.visibility = "hidden";
+    document.getElementById("menu6").innerHTML = "";
+    document.getElementById("menu6").style.visibility = "hidden";
     document.getElementById("menu1").innerHTML = '<h5>Persona</h5> <div class="row"> <table id="persona"> <tr> <th>Rol: </th> <td>Piloto </td> </tr> <tr> <th>Nombre: </th> <td>Illia</td> </tr> </table> </div> <h5>Piloto</h5> <div class="row"> <table id="piloto"> <tr> <th>Carnet: </th> <td>21312</td> </tr> <tr> <th>Certificados: </th> <td><a href="">EIS-123</a></td> </tr> <tr> <th>Persisos: </th> <td><a href="">GR11</a></td> </tr> <tr> <th>Formación: </th> <td>Prácticas</td> </tr> <tr> <th>Aeronaves: </th> <td>Marvin5, DJI Phantom</td> </tr> <tr> <th>Horas pilotadas: </th> <td>40 horas</td> </tr> </table> <img src="../img/flight_hours.png" alt="graph" style="width: 30%; height: 30%; margin-left: 25%; margin-top: 30px; margin-bottom: 30px;"> </div> <h5>Documentación</h5> <div class="row"> <table id="piloto"> <tr> <th>Seguro: </th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> <tr> <th>Certificados: </th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> </table> </div> <h5>Documentos adicionales</h5> <div class="row"> <table id="piloto"> <tr> <th></th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> <tr> <th></th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> </table> </div>';
     document.getElementById("menu1").style.visibility = "visible";
 }
@@ -214,6 +216,8 @@ function showAlumno() {
     document.getElementById("menu4").style.visibility = "hidden";
     document.getElementById("menu5").innerHTML = "";
     document.getElementById("menu5").style.visibility = "hidden";
+    document.getElementById("menu6").innerHTML = "";
+    document.getElementById("menu6").style.visibility = "hidden";
     document.getElementById("menu2").innerHTML = '<h5>Persona</h5> <div class="row"> <table id="persona"> <tr> <th>Rol: </th> <td>Alumno </td> </tr> <tr> <th>Nombre: </th> <td>Jesús</td> </tr> </table> </div> <h5>Alumno</h5> <div class="row"> <table id="piloto"> <tr> <th>Carnet: </th> <td>21312</td> </tr> <tr> <th>Certificados: </th> <td><a href="">EIS-123</a></td> </tr> <tr> <th>Persisos: </th> <td><a href="">GR11</a></td> </tr> <tr> <th>Formación: </th> <td>Prácticas</td> </tr> <tr> <th>Aeronaves: </th> <td>Marvin5, DJI Phantom</td> </tr> <tr> <th>Maniobra: </th> <td>Aterrizajes</td> </tr> <tr> <th>Referencia: </th> <td>23123</td> </tr> </table> <img src="../img/alumno_graph.jpg" alt="graph" style="width: 25%; height: 25%; margin-left: 25%; margin-top: 30px; margin-bottom: 30px;"> </div> ';
     document.getElementById("menu2").style.visibility = "visible";
 }
@@ -227,6 +231,8 @@ function showInstructor() {
     document.getElementById("menu4").style.visibility = "hidden";
     document.getElementById("menu5").innerHTML = "";
     document.getElementById("menu5").style.visibility = "hidden";
+    document.getElementById("menu6").innerHTML = "";
+    document.getElementById("menu6").style.visibility = "hidden";
     document.getElementById("menu3").innerHTML = '<h5>Persona</h5> <div class="row"> <table id="persona"> <tr> <th>Rol: </th> <td>Instructor </td> </tr> <tr> <th>Nombre: </th> <td>Pedro</td> </tr> </table> </div> <h5>Instructor</h5> <div class="row"> <table id="piloto"> <tr> <th>Carnet: </th> <td>21312</td> </tr> <tr> <th>Certificados: </th> <td><a href="">EIS-123</a></td> </tr> <tr> <th>Persisos: </th> <td><a href="">GR11</a></td> </tr> <tr> <th>Formación: </th> <td>Prácticas</td> </tr> <tr> <th>Aeronaves: </th> <td>Marvin5, DJI Phantom</td> </tr> <tr> <th>Horas pilotadas: </th> <td>40 horas</td> </tr> </table> <img src="../img/flight_hours.png" alt="graph" style="width: 30%; height: 30%; margin-left: 25%; margin-top: 30px; margin-bottom: 30px;"> </div> <h5>Documentación</h5> <div class="row"> <table id="piloto"> <tr> <th>Seguro: </th> <td><input style="width: 100%;" type="file" name="adjunto" accept=".*" /></td> </tr> <tr> <th>Certificados: </th> <td><input style="width: 100%;" type="file" name="adjunto" accept=".*" /></td> </tr> </table> </div> <h5>Documentos adicionales</h5> <div class="row"> <table id="piloto"> <tr> <th></th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> <tr> <th></th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> </table> </div>';
     document.getElementById("menu3").style.visibility = "visible";
 }
@@ -240,8 +246,25 @@ function showMecanico() {
     document.getElementById("menu3").style.visibility = "hidden";
     document.getElementById("menu5").innerHTML = "";
     document.getElementById("menu5").style.visibility = "hidden";
+    document.getElementById("menu6").innerHTML = "";
+    document.getElementById("menu6").style.visibility = "hidden";
     document.getElementById("menu4").innerHTML = '<h5>Persona</h5> <div class="row"> <table id="persona"> <tr> <th>Rol: </th> <td>Mecánico </td> </tr> <tr> <th>Nombre: </th> <td>Javier</td> </tr> </table> </div> <h5>Mecánico</h5> <div class="row"> <table id="piloto"> <tr> <th>Carnet: </th> <td>21312</td> </tr> <tr> <th>Certificados: </th> <td><a href="">EIS-123</a></td> </tr> <tr> <th>Persisos: </th> <td><a href="">GR11</a></td> </tr> <tr> <th>Formación: </th> <td>Prácticas</td> </tr> <tr> <th>Aeronaves: </th> <td>Marvin5, DJI Phantom</td> </tr> <tr> <th>Maniobra: </th> <td>Aterrizajes</td> </tr> <tr> <th>Referencia: </th> <td>23123</td> </tr> </table> <img src="../img/alumno_graph.jpg" alt="graph" style="width: 25%; height: 25%; margin-left: 25%; margin-top: 30px; margin-bottom: 30px;"> </div>';
     document.getElementById("menu4").style.visibility = "visible";
+}
+
+function showObservador() {
+    document.getElementById("menu2").innerHTML = "";
+    document.getElementById("menu2").style.visibility = "hidden";
+    document.getElementById("menu3").innerHTML = "";
+    document.getElementById("menu3").style.visibility = "hidden";
+    document.getElementById("menu4").innerHTML = "";
+    document.getElementById("menu4").style.visibility = "hidden";
+    document.getElementById("menu5").innerHTML = "";
+    document.getElementById("menu5").style.visibility = "hidden";
+    document.getElementById("menu1").innerHTML = "";
+    document.getElementById("menu1").style.visibility = "hidden";
+    document.getElementById("menu6").innerHTML = '<h5>Persona</h5> <div class="row"> <table id="persona"> <tr> <th>Rol: </th> <td>Observador </td> </tr> <tr> <th>Nombre: </th> <td>Illia</td> </tr> </table> </div> <h5>Observador</h5> <div class="row"> <table id="piloto"> <tr> <th>Carnet: </th> <td>21312</td> </tr> <tr> <th>Certificados: </th> <td><a href="">EIS-123</a></td> </tr> <tr> <th>Persisos: </th> <td><a href="">GR11</a></td> </tr> <tr> <th>Formación: </th> <td>Prácticas</td> </tr> <tr> <th>Aeronaves: </th> <td>Marvin5, DJI Phantom</td> </tr> <tr> <th>Horas pilotadas: </th> <td>40 horas</td> </tr> </table> <img src="../img/flight_hours.png" alt="graph" style="width: 30%; height: 30%; margin-left: 25%; margin-top: 30px; margin-bottom: 30px;"> </div> <h5>Documentación</h5> <div class="row"> <table id="piloto"> <tr> <th>Seguro: </th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> <tr> <th>Certificados: </th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> </table> </div> <h5>Documentos adicionales</h5> <div class="row"> <table id="piloto"> <tr> <th></th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> <tr> <th></th> <td><input style="width: 50%;" type="file" name="adjunto" accept=".*" multiple /><strong style="margin-left: 10%;">22/07/2020</strong></td> </tr> </table> </div>';
+    document.getElementById("menu6").style.visibility = "visible";
 }
 
 function showAddPersona() {
@@ -253,6 +276,8 @@ function showAddPersona() {
     document.getElementById("menu3").style.visibility = "hidden";
     document.getElementById("menu4").innerHTML = "";
     document.getElementById("menu4").style.visibility = "hidden";
+    document.getElementById("menu6").innerHTML = "";
+    document.getElementById("menu6").style.visibility = "hidden";
     document.getElementById("menu5").innerHTML = '<h5>Persona</h5> <div class="row"> <table id="persona"> <tr> <th>Rol: </th> <td> <select name="" id=""> <option value="">Piloto</option> <option value="">Alumno</option> <option value="">Instructor</option> <option value="">Mecánico</option> </select> </td> </tr> <tr> <th>Nombre: </th> <td><input type="text"></td> </tr> </table> </div> <h5>Datos</h5> <div class="row"> <table id="piloto"> <tr> <th>Carnet: </th> <td><input type="text"></td> </tr> <tr> <th>Certificados: </th> <td><input style="width: 100%;" type="file" name="adjunto" accept=".*" /></td> </tr> <tr> <th>Persisos: </th> <td><input style="width: 100%;" type="file" name="adjunto" accept=".*" /></td> </tr> <tr> <th>Formación: </th> <td><input type="text"></td> </tr> <tr> <th>Aeronaves: </th> <td><input type="text"></td> </tr> <tr> <th>Horas pilotadas: </th> <td><input type="text"></td> </tr> </table> </div> <h5>Documentación</h5> <div class="row"> <table id="piloto"> <tr> <th>Seguro: </th> <td><input style="width: 100%;" type="file" name="adjunto" accept=".*" /></td> </tr> <tr> <th>Certificados: </th> <td><input style="width: 100%;" type="file" name="adjunto" accept=".*" /></td> </tr> </table> </div> <div> <button type="submit" onclick="window.location.reload();">Guardar</button> </div>';
     document.getElementById("menu5").style.visibility = "visible";
 }
